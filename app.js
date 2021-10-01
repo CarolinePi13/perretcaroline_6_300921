42720b6d172c6d0a5c10e1cdd1bcd469b6c97b4a
CarolinePi13 <coraline.mo63@gmail.com>
2021-10-01 10:54:58 +0200

    users ok structure of sauces i/p

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 mongoose.connect(`mongodb+srv://Caropi13:cNVf5U2yiXDUJPUN@cluster0.j0rpp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  .then(() => console.log('Connexion to MongoDB failed !'))
+  .catch(() => console.log('Connexion to MongoDB was successful!'));
+
 app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+   res.json({ message: 'Your request was successful !' }); 
 });
 app.use(express.json());
+
+
+app.use('/api/auth', userRoutes);
+
 module.exports = app;
---
+++ models/sauce.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({
+    userId:{type:String},
+    name:{type:String},
+    manufacturer:{type:String},
+    description:{type:String},
+    mainPepper:{type:String},
+    imageUrl:{type:String},
+    heat:{type:Number},
+    likes:{type:Number},
+    dislikes:{type:Number},
+    
+})
+
+modukle.exports = mangoose.model("Sauce", sauceSchema);
